9ac3d39cd4bdca85fb9b6987f9bed0f70cbeb0ee
Marwen_Ben_Aouicha <marwen.benaouicha@enis.tn>
2022-03-01 12:08:00 +0100

    Ajoutez des fichiers projet.

---
+++ BookStore-API/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
