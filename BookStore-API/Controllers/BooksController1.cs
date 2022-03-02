0e46843218ea9afd623fcd8b483e6f960260b1ee
Marwen_Ben_Aouicha <marwen.benaouicha@enis.tn>
2022-03-02 11:26:31 +0100

    Author Repository-Service-Controller

---
+++ BookStore-API/Controllers/BooksController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Controllers
+{
+    public class BooksController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
